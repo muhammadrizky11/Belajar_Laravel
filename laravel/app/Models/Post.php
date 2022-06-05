2bbabc9b948c828e1fea37810590e789d0a6f5cb
bangaco123 <acobaba422@gmail.com>
2022-06-06 01:51:13 +0700

    menabh database

---
+++ laravel/app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\post as ModelsPost;
+use App\Models\Post as AppModelsPost;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    //cara mudah buat baru data post
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    //cara supata tidak menbahi satu satu
+    protected $guarded = ['id',];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
